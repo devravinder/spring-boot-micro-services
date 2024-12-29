74fedc4e8205f41a179a83f68db4a6e88cc5719f
Ravinder Reddy Kothabad <developer.ravinder.reddy@gmail.com>
2024-12-30 00:06:29 +0530

    #92146295 feat: Catalog service module setup

---
+++ catalog-service/src/test/java/com/paravar/TestCatalogServiceApplication.java
@@ -0,0 +1,18 @@
+package com.paravar;
+
+import org.springframework.boot.SpringApplication;
+
+/*
+ This is to run the application in the test container ( for local development )
+  - this is useful to run the current service with all required containers
+     - without running entire application containers
+
+*/
+public class TestCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(CatalogServiceApplication::main)
+                .with(ContainersConfig.class)
+                .run(args);
+    }
+}
